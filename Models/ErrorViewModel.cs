24e01d76a4d865ab7b6eae6b068555d5a9d1b062
emiliano.bonilla <emiliano@decemberlabs.com>
2021-12-08 10:48:22 -0300

    Add MVC Core Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitMvCommand.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
